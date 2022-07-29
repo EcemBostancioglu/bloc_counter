543b547faebe84f8b0677c1833f792686777dacf
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-07-30 01:52:06 +0300

    Created IncrementCounterEvent

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -6,10 +6,16 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
 
+  int value=0;
 
   CounterBloc() : super(CounterInitial()) {
     on<CounterEvent>((event, emit) {
       // TODO: implement event handler
+    });
+
+    on<IncrementCounterEvent>((event,emit){
+      value+=1;
+      emit(IncrementCounterState(value));
     });
 
   }
