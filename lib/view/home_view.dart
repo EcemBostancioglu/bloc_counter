172efbca5e6e7e3e6e07eed95f367a726b121d91
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-07-30 01:33:01 +0300

    App Init

---
+++ lib/view/home_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text('0'),
+      ),
+    );
+  }
+}
